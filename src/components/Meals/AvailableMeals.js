d565b423376455e846089f8c3933676fb4bc9936
cross-sport <63291426+cross-sport@users.noreply.github.com>
2023-05-10 21:18:03 +0400

    loading

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const responseData = await (
+        await fetch(
+          "https://react-http-71448-default-rtdb.firebaseio.com/meals.json"
+        )
+      ).json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      }
+    };
+    fetchMeals();
+  }, []);
+  // loading
+  if (isLoading) {
+    return (
+      <section className={classes.mealIsLoading}>
+        <p>Loading . . .</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
